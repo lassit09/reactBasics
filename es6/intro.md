dc54d154771ae51891e0a7d4dfc5e8308f60b281
Taylor Lassiter <lassit09@gmail.com>
2018-04-23 21:45:11 -0400

    practice and learn es6

--- es6/intro.md
+++ es6/intro.md
@@ -1,3 +1,16 @@
-BABEL
+ECMAScript6 (ES6)
 -------
 * We can use Babel to convert ES6 code to ES5 and vice versa.
+* var is function-scoped
+* let is block-scoped
+    * let won't let you use the same variable name
+    * let can be updated
+    * should use let more than const
+* const is block-scoped
+    * const cannot be reassigned
+    * if const is an object, it is mutable
+        * can't change the value of the object but can change it's properties
+
+ARROW FUNCTIONS
+-----------------
+
--- index.js
+++ index.js
@@ -1,2 +1,45 @@
-let greeting = 'Hello world';
-console.log(greeting);
+// let greeting = 'Hello world';
+// console.log(greeting);
+
+// var name = "Taylor";
+// console.log(name);
+// name = "Kayla";
+// console.log(name);
+// prints Taylor and then reassigns the variable and prints Kayla
+
+// function printName() {
+//     var name = "Kayla";
+//     console.log(name);
+// }
+// printName();
+// prints Kayla
+
+// var name = "Taylor";
+// if (name == "Taylor") {
+//     let fullName = "Taylor Lassiter";
+//     const age = 26;
+// }
+// console.log(age);
+// gives an error because const is block-scoped
+
+const integers = [1,2,3]; //map to make [2,3,4]
+
+// const updatedIntegers = integers.map(function(number) {
+//     return number += 1
+// });
+
+//normal arrow function
+// const updatedIntegers = integers.map((number) => {
+//     return number += 1
+// });
+
+//implicit return arrow function
+// const udatedIntegers = integers.map(number => number += 1);
+
+// console.log(updatedIntegers);
+
+const ages = [22, 43, 6, 17, 35];
+
+const adults = ages.filter(age => age > 20);
+
+console.log(adults);
